2db45c651660753312e14319d61870c66197275b
Sa4rg <sa30811670@gmail.com>
2025-12-19 23:46:55 +0000

    chore(api): align tests and error middleware before first feature

--- apps/api/src/middlewares/error.middleware.js
+++ apps/api/src/middlewares/error.middleware.js
@@ -2,7 +2,9 @@ const { fail } = require('../utils/response');
 
 function errorMiddleware(err, req, res, next) {
   console.error(err.stack);
-  fail(res, err.message || 'Internal Server Error', 500);
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  fail(res, message, statusCode);
 }
 
 module.exports = errorMiddleware;
