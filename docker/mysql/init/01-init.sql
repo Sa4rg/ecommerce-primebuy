d30147b0c1e5eb43450aaca360e183ff455fa88b
Sa4rg <sa30811670@gmail.com>
2026-02-05 16:57:07 +0000

    feat(env): update environment variable defaults and add web API base URL; remove obsolete web .gitignore

--- docker/mysql/init/01-init.sql
+++ docker/mysql/init/01-init.sql
@@ -1,13 +1,11 @@
--- MySQL Initialization Script
--- Creates both development and test databases
--- Grants privileges to ecommerce user on both databases
+-- Create development database
+CREATE DATABASE IF NOT EXISTS ecommerce_dev;
 
 -- Create test database
 CREATE DATABASE IF NOT EXISTS ecommerce_test;
 
--- Grant privileges to ecommerce user on both databases
+-- Grant privileges
 GRANT ALL PRIVILEGES ON ecommerce_dev.* TO 'ecommerce'@'%';
 GRANT ALL PRIVILEGES ON ecommerce_test.* TO 'ecommerce'@'%';
 
--- Apply changes
 FLUSH PRIVILEGES;
